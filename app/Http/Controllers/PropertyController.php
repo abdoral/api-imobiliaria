2e226d96d161344475d942c58a8983f494d3c185
Abdoral Timóteo <abdoraltimoteo@sobral.ce.gov.br>
2022-04-01 10:38:54 -0300

    Adicionado as funcionalidades de atualizar e remover imoveis

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -50,12 +50,52 @@ class PropertyController extends Controller
 
     public function update(Request $request){
 
+        $request->validate([
+            'id' => 'required|integer|exists:property,id',
+            'name' => 'required|string|max:255',
+            'type' => 'required',
+            'rent_value' => 'required|numeric',
+            'sale_value' => 'required|numeric',
+            'cep' => 'required|formato_cep',
+            'state' => 'required|uf',
+            'city' => 'required|string|max:255',
+            'street' => 'required|string|max:255',
+            'number' => 'required|string|max:10',
+            'district' => 'required|string|max:255',
+            'complement' => 'string|max:255',
+            'country' => 'required|string|max:255',
+            'company_id' => 'required|exists:company,id',
+            'property_owners_id' => 'required|array|exists:property_owner,id'
+        ]);
+
+        $property = Property::find($request->id);
+        $property->update($request->all());
+        $property->fresh();
+        $ownersProperty = OwnersProperty::where('property_id', $request->id);
+        $ownersProperty->delete();
+        foreach ($request->property_owners_id as $propery_owner) {
+            OwnersProperty::create(
+                [
+                'property_id' => $property->id,
+                'property_owner_id' => $propery_owner
+                ]
+            );
+        }
+
+        return response()->json([
+            'message' => 'Imóvel atualizado com sucesso.',
+            'company' => $property
+        ], 200);
+
     }
 
     public function delete(Request $request){
         $request->validate([
             'id' => 'required|integer|exists:property,id'
         ]);
+
+        $ownersProperty = OwnersProperty::where('property_id', $request->id);
+        $ownersProperty->delete();
 
         Property::destroy($request->id);
 
